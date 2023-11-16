e22807cf6fc315c7ada20d4420c8f3db5d68d1e6
divinoschaeffer <divino.schaeffer02@gmail.com>
2023-11-16 16:56:26 +0100

    add route getAllPosts

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {update, getAll, getUser} = require('../controllers/user')
+const {update, getAll, getUser, getAllPosts} = require('../controllers/user')
 const verifTokenUser = require('../middlewares/token')
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/getAll',verifTokenUser, getAll); // récupérer tous les utilisateurs
 router.get('/:idName', verifTokenUser, getUser); // récupérer un utilisateur 
 router.put('/update/:idName',verifTokenUser, update); // mettre à jour un utilisateur
+router.get('/getAllPosts/:user', getAllPosts);
 
 module.exports = router;
