2e17785421db9396998d7e12bdbb1e1e6251118c
faschaeffer <divino.schaeffer02@gmail.com>
2023-11-21 17:04:02 +0100

    refactor and improvement sign-in and login

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -6,54 +6,62 @@ const cookieParser = require('cookie-parser')
 
 // Enregistrement d'un nouvel utilisateur
 async function signIn(req, res) {
-    try {
+    
+    const existingUser = await User.findOne({idName: req.body.idName});
+    if(existingUser)
+        return res.status(409).json("Nom d'utilisateur déjà pris");
 
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    let token;
 
-        const user = new User({
-            username: req.body.username,
-            idName: req.body.idName,
-            password: hashedPassword,
-            birthDate: new Date(req.body.birthDate),
-            firstConnection: new Date(),
-            public: req.body.public
-        })
-
-        try {
-            
-            const secretKey = process.env.TOKEN_KEY;
-            const data = {
-                user: {
-                    username: req.body.username,
-                    idName: req.body.idName,
-                    public: req.body.public,
-                    role: req.body.role
-                }
-            };
-            const token = jwt.sign(data, secretKey);
-            res.cookie('token', token, {httpOnly: true});
-        } catch (error) {
-            res.status(500).json(console.log(error))
-        }
+    const user = new User({
+        username: req.body.username,
+        idName: req.body.idName,
+        password: hashedPassword,
+        birthDate: new Date(req.body.birthDate),
+        firstConnection: new Date(),
+        public: req.body.public
+    })
         
-        user.save()
-            .then((savedUser) => {
-                res.status(201).json(savedUser);;
-            })
-            .catch((err) => {
-                console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', err);
-            });
+    
+    user.save()
+        .then((savedUser) => {
 
-    } catch (error) {
-        res.status(500).json("Erreur lors de la création de l'utilisateur");
-    }
+            const secretKey = process.env.TOKEN_KEY;
+            token = jwt.sign({
+                _id: req.body._id,
+                idName: req.body.idName,
+                public: req.body.public,
+                role: req.body.role
+            }, secretKey
+            ,{expiresIn: '10h'}
+            );
+
+            res.cookie('token', token, {httpOnly: true});
+
+            res.status(201).json({
+                token, 
+                user:
+                {
+                    id: savedUser._id,
+                    idName: savedUser.idName, 
+                    public: savedUser.public, 
+                    role: savedUser.role}
+                })
+            })
+        .catch((err) => {
+            console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', err);
+        });
+
 }
 
 // Connexion d'un utilisateur
 async function login(req, res){
     const {idName, password} = req.body;
     let loggedUser;
+    let token;
+
     await User.findOne({idName})
         .then((user) => loggedUser = user)
         .catch((err) =>  {
@@ -63,24 +71,25 @@ async function login(req, res){
 
     await bcrypt.compare(password, loggedUser.password)
         .then(() => {
-            try {
-            
-                const secretKey = process.env.TOKEN_KEY;
-                const data = {
-                    user: {
-                        username: loggedUser.username,
-                        idName: loggedUser.idName,
-                        public: loggedUser.public,
-                        role: loggedUser.role
-                    }
-                };
-                const token = jwt.sign(data, secretKey);
-                res.cookie('token', token, {httpOnly: true});
-            } catch (error) {
-                res.status(500).json(console.log(error))
-            }
-            res.status(201).json("Authentification réussie")
-        })
+            const secretKey = process.env.TOKEN_KEY;
+            token = jwt.sign({
+                _id: req.body._id,
+                idName: req.body.idName,
+                public: req.body.public,
+                role: req.body.role
+            }, secretKey
+            ,{expiresIn: '10h'}
+            );
+            res.status(201).json({
+                token, 
+                user:
+                {
+                    id: loggedUser._id,
+                    idName: loggedUser.idName, 
+                    public: loggedUser.public, 
+                    role: loggedUser.role}
+                })
+            })
         .catch((err) => {
             console.log(err);
             res.status(404).json("L'authentification a échoué");
