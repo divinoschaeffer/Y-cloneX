f88163c16b7380143cabb2ac68719f902659968c
divinoschaeffer <divino.schaeffer02@gmail.com>
2023-11-24 11:53:09 +0100

    login input

--- front/src/components/LoginModal.jsx
+++ front/src/components/LoginModal.jsx
@@ -1,6 +1,10 @@
 import {react, useState} from "react";
+import axios from 'axios'
 
 const LoginModal = ({showModal, closeModal}) => {
+
+    const [input, setInput] = useState("");
+
     return(
         <div 
         className={`fixed inset-0 bg-white md:bg-gray-800 md:bg-opacity-50 flex md:items-center md:justify-center w-full
@@ -15,7 +19,14 @@ const LoginModal = ({showModal, closeModal}) => {
                 </section> 
                 <section className="flex flex-col items-center justify-center space-y-6">
                     <h1 className="font-bold text-2xl md:text-3xl">Connectez-vous à Y</h1>
-                    <input className="border w-3/4 md:w-1/2 h-10 focus:border-twitter-blue focus:outline-none focus:border-2 rounded px-2" placeholder="Nom d'utilisateur" ></input>
+                    <input className="border w-3/4 md:w-1/2 h-10 focus:border-twitter-blue focus:outline-none focus:border-2 rounded px-2" 
+                    placeholder="Nom d'utilisateur" 
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    >
+
+                    </input>
                     <button className="text-white font-bold bg-black rounded-full w-3/4 md:w-1/2 h-10">Suivant</button>
                     <button className="font-bold bg-white rounded-full border-gray-300 border w-3/4 md:w-1/2 h-10">Mot de passe oublié ?</button>
                     <div className="flex flex-row">
