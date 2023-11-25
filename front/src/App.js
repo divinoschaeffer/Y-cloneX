4ccc04036f9efd9572b2eb0c73c8aeb1ada52f41
divinoschaeffer <divino.schaeffer02@gmail.com>
2023-11-25 12:10:46 +0100

    new route

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../src/pages/Home"
+import { AuthProvider } from './context/AuthContext';
+import Setup from "./pages/Setup"
+import Home from './pages/Home';
 
 function App() {
   
   return (
+    <AuthProvider>
       <BrowserRouter >
         <Routes>
-          <Route path='/' exact Component={Home}></Route>
+          <Route path='/' exact Component={Setup}></Route>
+          <Route path='/home' Component={Home}></Route>
         </Routes>
       </BrowserRouter>
+    </AuthProvider> 
   );
 }
 
