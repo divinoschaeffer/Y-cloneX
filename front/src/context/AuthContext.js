43455d3d3b76b66b96227964e296f925bb51acd4
faschaeffer <divino.schaeffer02@gmail.com>
2024-02-06 15:23:28 +0100

    store user in localstorage

--- front/src/context/AuthContext.js
+++ front/src/context/AuthContext.js
@@ -4,13 +4,16 @@ import { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 export function AuthProvider({children}){
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(localStorage.getItem("user"));
 
     const login = (userData) => {
+        console.log(userData);
+        localStorage.setItem("user",JSON.stringify(userData));
         setUser(userData);
     }
 
     const logout = () => {
+        localStorage.setItem("user", JSON.stringify({user: null}));
         setUser(null);
     }
 
