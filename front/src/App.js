02c2e02e36b682a2a17fc0131458300e7a1d426d
faschaeffer <divino.schaeffer02@gmail.com>
2024-02-17 17:11:26 +0100

    add route /post/:id

--- front/src/App.js
+++ front/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import Setup from "./pages/Setup"
 import Home from './pages/Home';
+import Post from './pages/Post';
 
 function App() {
   
   return (
     <AuthProvider>
-      <BrowserRouter >
+      <BrowserRouter>
         <Routes>
           <Route path='/' exact Component={Setup}></Route>
           <Route path='/home' Component={Home}></Route>
+          <Route path='/post/:id' element={<Post></Post>}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider> 
